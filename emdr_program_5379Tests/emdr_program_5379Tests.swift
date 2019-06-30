6dc534a5b7cedca0e6d3d398d7834671c3b4cdff
Crowdbotics <team@crowdbotics.com>
2019-06-30 04:09:21 +0000

    initial commit

---
+++ emdr_program_5379Tests/emdr_program_5379Tests.swift
@@ -0,0 +1,34 @@
+//
+//  emdr_program_5379Tests.swift
+//  emdr_program_5379Tests
+//
+//  Created by Dan on 10/23/18.
+//  Copyright © 2018 Crowdbotics. All rights reserved.
+//
+
+import XCTest
+@testable import emdr_program_5379
+
+class emdr_program_5379Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
